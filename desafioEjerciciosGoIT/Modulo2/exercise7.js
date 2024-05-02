6b18c8a2bb2745c8ecfbdeec025238cb46552cc1
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2024-05-01 19:43:50 -0600

    exercise7

---
+++ desafioEjerciciosGoIT/Modulo2/exercise7.js
@@ -0,0 +1,12 @@
+/**
+ * Declare una variable fruitsArrayLength y asignele la longitud del array fruits usando la propiedad length.
+
+Declare la variable fruitsArrayLength
+El valor de la variable fruitsArrayLength del número 4
+ */
+
+const fruits = ["apple", "plum", "pear", "orange"];
+
+const fruitsArrayLength = fruits.length;
+
+console.log(fruitsArrayLength);
