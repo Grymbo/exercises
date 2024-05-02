2482fb679d8b93b4cff9a1e1a85f00a499ca84a1
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2024-05-01 19:47:55 -0600

    exercise8

---
+++ desafioEjerciciosGoIT/Modulo2/exercise8.js
@@ -0,0 +1,19 @@
+/**
+ * Declare dos variables:
+
+Nombre de la variable	Valor esperado
+lastElementIndex	El índice del último elemento del array fruits a través de longitud_del_array- 1
+lastElement	El valor del último elemento del array
+Declare la variable lastElementIndex
+El valor de la variable lastElementIndex del número 3
+Declare la variable lastElement
+El valor de la variable lastElement del número "banana"
+ */
+
+const fruits = ["apple", "plum", "pear", "orange"];
+
+const lastElementIndex = fruits.length - 1;
+const lastElement = fruits[fruits.length -1];
+
+console.log(lastElementIndex);
+console.log(lastElement);
