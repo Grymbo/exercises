304db6b752b364956ce9bc4600fc395738dc7543
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2024-05-01 19:38:22 -0600

    exercise 6

---
+++ desafioEjerciciosGoIT/Modulo2/exercise6.js
@@ -0,0 +1,12 @@
+/**
+ * Redefina a los valores de los elementos indexados 1 y 3. Sustituya "plum" por "peach", y "orange" por "banana".
+
+Declare la variable fruits
+El valor de la variable fruits es un array ["apple", "peach", "pear", "banana"]
+ */
+const fruits = ["apple", "plum", "pear", "orange"];
+
+fruits[1] = "peach";
+fruits[3] = "banana";
+
+console.log(fruits);
