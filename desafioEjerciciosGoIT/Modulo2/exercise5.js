8e2ce1694161dbc99eee4136d3445544c826715d
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2024-05-01 19:31:00 -0600

    exercise 5

---
+++ desafioEjerciciosGoIT/Modulo2/exercise5.js
@@ -0,0 +1,25 @@
+/**
+ * Declare tres variables y asigna un valor a cada una de ellas usando la notación de corchetes.
+
+Nombre de la varaible	Valor de la variable
+firstElement	primer elemento array
+secondElement	segundo elemento array
+lastElement	último elemento array
+Declare la variable firstElement
+El valor de la variable firstElement de la secuencia "apple"
+Declare la variable secondElement
+El valor de la variable secondElement de la secuencia "plum"
+Declare la variable lastElement
+El valor de la variable lastElement de la secuencia "orange"
+ */
+
+const fruits = ["apple", "plum", "pear", "orange"];
+
+let firstElement = fruits[0];
+let secondElement = fruits[1];
+let lastElement = fruits[fruits.length - 1];
+
+console.log(fruits);
+console.log(firstElement);
+console.log(secondElement);
+console.log(lastElement);
