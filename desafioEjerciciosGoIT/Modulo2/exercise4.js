016c2094946541e51708f18a49abbdd03c147650
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2024-05-01 18:50:07 -0600

    exercise4

---
+++ desafioEjerciciosGoIT/Modulo2/exercise4.js
@@ -0,0 +1,10 @@
+/**
+ * Declare la variable fruits y asígnele un array de frutas, la secuencia es "apple", "plum", "pear" y "orange".
+
+Declare la variable fruits
+El valor de la variable fruits es un array ["apple", "plum", "pear", "orange"]
+ */
+
+const fruits = ["apple", "plum", "pear", "orange"];
+
+console.log(fruits);
