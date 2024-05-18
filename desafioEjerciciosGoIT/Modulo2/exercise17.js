2505da5b01bdedde43ff79312fe8b6de8f66fdc2
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2024-05-17 22:01:44 -0600

    exercise17

---
+++ desafioEjerciciosGoIT/Modulo2/exercise17.js
@@ -0,0 +1,19 @@
+/**
+ * Complete el ciclo for para que registre todos los números enteros entre start y end respectivamente.
+
+Declare la variable start
+El valor de la variable start del número 3
+Declare la variable end
+El valor de la variable end del número 7
+Declare la variable i - recuento de iteraciones
+El valor inicial de la variable i es 3.
+La condición del ciclo se establece como true siempre que i sea menor o igual a 7.
+En cada iteración, el valor de la variable i se incrementa en uno
+La emisión de la variable i a la consola mostrará los números 3, 4, 5, 6, 7.
+ */
+var star = 3;
+var end = 7;
+
+for(let i = star; i <= end; i++){
+    console.log(i);
+}
