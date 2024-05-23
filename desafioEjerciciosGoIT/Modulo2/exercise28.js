7cf1e5cf6be3361278aa7288e67d11c5d95bb605
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2024-05-22 20:48:45 -0600

    exercise28

---
+++ desafioEjerciciosGoIT/Modulo2/exercise28.js
@@ -0,0 +1,20 @@
+/**
+ * Complete las expresiones de resto de división para que el código pase las pruebas.
+
+Declare la variable a
+El valor de la variable a del número 0
+Declare la variable b
+El valor de la variable b del número 1
+Declare la variable c
+El valor de la variable c del número 3
+Declare la variable d
+El valor de la variable d del número 5
+Declare la variable e
+El valor de la variable e del número 2
+ */
+
+const a = 3 % 3;
+const b = 4 % 3;
+const c = 11 % 4;
+const d = 12 % 7;
+const e = 8 % 3;
