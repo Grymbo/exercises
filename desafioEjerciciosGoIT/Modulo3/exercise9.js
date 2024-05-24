5296ba18124819282094e81990c9fd9294bb1e4f
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2024-05-23 22:47:25 -0600

    exercise9

---
+++ desafioEjerciciosGoIT/Modulo3/exercise9.js
@@ -0,0 +1,23 @@
+/**
+ * Complete el código de declaración del objeto credentials para que tenga dos propiedades: email y password en donde los nombres se almacenan en las variables emailInputName y passwordInputName.
+
+El valor de la propiedad email debe ser la secuencia "henry.carter@aptmail.com" y el valor de la propiedad password debe ser la secuencia "jqueryismyjam".
+
+Declare la variable credentials
+El valor de la variable credentials del objeto
+El objeto credentials tiene una propiedad email
+El valor de la propiedad anidada email de la secuencia "henry.carter@aptmail.com"
+El obketo credentials tiene una propiedad password
+El valor de la propiedad anidada password es la secuencia "jqueryismyjam"
+ */
+const emailInputName = "email";
+const passwordInputName = "password";
+
+const credentials = {
+    [emailInputName]: "henry.carter@aptmail.com",
+    [passwordInputName]: "jqueryismyjam"
+};
+
+
+
+console.log(credentials);
