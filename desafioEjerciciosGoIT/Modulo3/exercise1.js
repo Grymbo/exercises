02e023b400fc5b4396eaa55e41d8e9923d21e018
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2024-05-23 20:57:39 -0600

    1

---
+++ desafioEjerciciosGoIT/Modulo3/exercise1.js
@@ -0,0 +1,29 @@
+/**
+ * Asigne a la variable apartment un objeto que describa un piso con las siguientes características:
+
+imgUrl - es una fotografía, valor "https://via.placeholder.com/640x480";
+descr - decripción, valor "Spacious apartment in the city center";
+rating - rating, valor 4;
+price - precio, valor 2153;
+tags - meta-información, array ["premium", "promoted", "top"].
+Declare la variable apartment
+El valor de la variable apartment del objeto
+El objeto tiene una propiedad imgUrl
+El valor de la variable imgUrl de la secuencia "https://via.placeholder.com/640x480"
+El objeto tiene una propiedad descr
+El valor de la variable descr de la secuencia "Spacious apartment in the city center"
+El objeto tiene una propiedad rating
+El valor de la variable rating del número 4
+El objeto tiene una propiedad price
+El valor de la variable price del número 2153
+El objeto tiene una propiedad tags
+El valor de la variable tags del array ["premium", "promoted", "top"]
+ */
+const apartment = {
+    imgUrl: "https://via.placeholder.com/640x480",
+    descr: "Spacious apartment in the city center",
+    rating: 4,
+    price: 2153,
+    tags: ["premium", "promoted", "top"]
+};
+console.log(apartment);
