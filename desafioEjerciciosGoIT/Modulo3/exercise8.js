d28d2aa6356a1b53b8b50549968797965abeb68a
Javier Gonzalez <javiergonzalezupvm@gmail.com>
2024-05-23 22:34:39 -0600

    exercise8

---
+++ desafioEjerciciosGoIT/Modulo3/exercise8.js
@@ -0,0 +1,25 @@
+/**
+ * Complete el código de declaración del objeto para que tenga las propiedades name, price, image и tags con los valores de las variables con nombres similares. Asegúrese de usar la sintaxis breve de las propiedades.
+
+Declare la variable product
+El valor de la variable product del objeto
+El valor de la propiedad anidada name de la secuencia "Repair Droid"
+El valor de la propiedad anidada price del número 2500
+El valor de la propiedad anidada image de la secuencia "https://via.placeholder.com/640x480"
+El valor de la propiedad anidada tags del array ["on sale", "trending", "best buy"]
+ */
+
+
+const name = "Repair Droid";
+const price = 2500;
+const image = "https://via.placeholder.com/640x480";
+const tags = ["on sale", "trending", "best buy"];
+
+const product = {
+    name,
+    price,
+    image,
+    tags
+};
+
+console.log(product);
